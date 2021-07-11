7f02a791a1705820b15537faf62d9f2a366854b4
CuzzImBatman <lgbao19@apcs.vn>
2021-07-11 11:43:01 +0700

    .

--- app/src/main/java/com/example/eflashcard/Word.java
+++ app/src/main/java/com/example/eflashcard/Word.java
@@ -1,9 +1,10 @@
 package com.example.eflashcard;
 
+import android.content.Context;
+
 public class Word {
     String word;
     String definition;
-
     public Word(String word, String definition) {
         this.word = word;
         this.definition = definition;
@@ -13,15 +14,22 @@ public class Word {
         return word;
     }
 
-    public void setWord(String word) {
-        this.word = word;
-    }
-
     public String getDefinition() {
         return definition;
     }
 
-    public void setDefinition(String definition) {
-        this.definition = definition;
+    // Get the image ID in the drawable folder
+    public int getImageID(Context context) {
+        return context.getResources().getIdentifier(word.toLowerCase().replace(' ', '_'),
+                "drawable",
+                context.getPackageName());
+    }
+
+    // Get the audio ID in the raw folder
+    public int getAudioID(Context context) {
+        word = word.toLowerCase().replace(' ', '_');
+        return context.getResources().getIdentifier(word.toLowerCase().replace(' ', '_'),
+                "raw",
+                context.getPackageName());
     }
 }
